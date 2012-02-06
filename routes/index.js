880bf6c99dcb0df05e62cda6025a08185a1e830a
Marco Cullen <marco.cullen@gmail.com>
2012-02-06 01:19:52 +0000

    got datatables zero config working next up, json request

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 
 /*
- * GET home page.
+* GET home page.
  */
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+exports.table = function(req, res) {
+	res.sendfile('test.html');
+}
